739872215892f46ff71a49d5606f8acc40013b3f
James Rae <jrae151515@yahoo.co.uk>
2022-01-21 18:17:18 +0000

    added random color mode button and added functioanlity for this, also updated general styling

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //slider
 let values = [36,49,64,81,100,121,144,169,196,225]; 
-
 let input = document.getElementById('input'),
     output = document.getElementById('output');
 
 let numberOfBlocks;
+
+//color button
+let colorToggle = 0;
+const colorButton = document.getElementById("colorButton");
+colorButton.addEventListener("click", () => {
+    if (colorToggle === 0) {
+        colorToggle = 1;
+        generateGrid();
+    }
+    else {
+        colorToggle = 0;
+        generateGrid();
+    }
+});
 
 input.oninput = function(){
     output.innerHTML = values[this.value];
@@ -13,30 +26,43 @@ input.oninput = function(){
 };
 input.oninput(); //set default value
 
-
-
 function generateGrid() {
     const oldgrid = document.getElementById('gridContainer');
     oldgrid.innerHTML = '';
-    
     for (let x = 0; x < numberOfBlocks; x++) {
         let block = document.createElement('div');
         block.className = 'block';
         document.getElementById('gridContainer').appendChild(block);
     }
-
     for (const grid of document.querySelectorAll('#gridContainer')) {
         grid.style.setProperty('--cols', Math.ceil(Math.sqrt(numberOfBlocks)));
     }
-    
-    //set block black with mouseover
-    let block = document.querySelectorAll('.block');
 
-    block.forEach((block) => {
-        block.addEventListener("mouseover", () => {
-            block.setAttribute("style", "background-color:black;")
-        });
-    });  
+    function randomColor() {
+        let letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    //set block black/color with mouseover
+    const block = document.querySelectorAll('.block');
+    if (colorToggle === 0) {
+        block.forEach((block) => {
+            block.addEventListener("mouseover", () => {
+                block.style.backgroundColor = "black";
+            });
+        });  
+    }
+    else {
+        block.forEach((block) => {
+            block.addEventListener("mouseover", () => {
+                block.style.backgroundColor = randomColor();
+            });
+        });  
+    }
 
     //clear button
     const clearButton = document.getElementById("clearButton");
@@ -46,7 +72,20 @@ function generateGrid() {
         });
     });  
 
+    //grid management button
+    const gridButton = document.getElementById("gridButton");
+    block.forEach((block) => {
+        gridButton.addEventListener("click", () => {
+           block.classList.toggle("block");
+        });
+    });
 }
+
+
+
+
+ 
+
 
    
 
